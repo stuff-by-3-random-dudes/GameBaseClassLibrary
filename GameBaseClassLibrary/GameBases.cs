5442f51a57bc922e9f21eab6c7845d55930ad28e
NicoAICP <nicohaberkorn@gmx.de>
2020-04-09 00:49:49 +0200

    Projektdateien hinzufügen.

---
+++ GameBaseClassLibrary/GameBases.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBaseClassLibrary
+{
+    [Serializable]
+    public class GameBases
+    {
+        public string Name { get; set; }
+        public Regions Region { get; set; }
+        public string Path { get; set; }
+        public string Tid { get; set; }
+        public int KeyHash { get; set; }
+    }
+}
---
+++ GameBaseClassLibrary/Regions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBaseClassLibrary
+{
+    public enum Regions
+    {
+        EU,
+        US,
+        JP
+    }
+    public enum GameConsoles
+    {
+        NDS, N64, GBA, NES, SNES, TG16, MSX
+    }
+}
+
